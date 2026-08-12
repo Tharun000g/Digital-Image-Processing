0a3a45a25c5503cda4445371c6ae7d42b059f251
Tharun Geddam <tharun000g@gmail.com>
2026-08-12 19:25:33 +0530

    Add RGB to grayscale conversion and image

--- Lab-1/RGB2GREY.py
+++ Lab-1/RGB2GREY.py
@@ -1,1 +1,19 @@
-hlo
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the image
+image = plt.imread("emojis.png")
+
+# Get the RGB channels
+R = image[:, :, 0]
+G = image[:, :, 1]
+B = image[:, :, 2]
+
+# Use only the Red channel
+gray = R
+
+# Display the grayscale image
+plt.imshow(gray, cmap="gray")
+plt.axis("off")
+plt.show()
